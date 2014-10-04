1c545dd2315aee70358ae9d5fe521110384d3c74
Ratkin Sergey <greshnik-sv@yandex.ru>
2014-10-04 13:08:22 +0300

    Working with ProcessPriceWorker

--- Helper/ProcessPriceWorker.php
+++ Helper/ProcessPriceWorker.php
@@ -8,11 +8,11 @@ class ProcessPriceWorker
     }
 
     public function run(){
-        global $db,$PHP;
+        global $PHP;
 
         $outputfile="/var/www/Medic/out.txt";
         $pidfile="pid.txt";
-        exec(sprintf("%s > %s 2>&1 & echo $! >> %s",$PHP." ".__FILE__, $outputfile, $pidfile));
+        exec(sprintf("%s > %s 2>&1 & echo $! >> %s",$PHP." ".__FILE__." {$this->id} {$this->file_name} ", $outputfile, $pidfile));
     }
 }
 
@@ -20,5 +20,54 @@ if(!isset($PHP))
 {
    // Do process file
 
+    require_once 'Helper/UUID.php';
+    require_once 'config.php';
 
+
+    /*require_once 'Helper/Database.php';
+    $db = new Database($DB_HOST, $DB_NAME, $DB_USER, $DB_PASS);
+    $db -> Connect();
+
+    $db->Exec("INSERT INTO Products(id,Number,NumberProvider,Name,FullName,BasicCharacteristics,ProviderId,Price,Rest) VALUE ('cc9cce8c-1afb-4151-8612-d5b91589c3d8','0','0','0','0','0','ba8b733a-5154-4e7a-aeaa-f9d90258a238','9','9');");
+*/
+
+    $dbh;
+    try {
+        $dbh = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS);
+        $dbh->beginTransaction();
+        //$sth = $dbh->exec("DROP TABLE fruit");
+
+
+        $sql = "INSERT INTO `Products`(`id`,`Number`,`NumberProvider`,`Name`,`FullName`,`BasicCharacteristics`,`ProviderId`,".
+            "`Price`,`Rest`) VALUE (?,?,?,?,?,?,?,?,?);";
+        $sql2 = "INSERT INTO `ProductsSearch`(`ProductId`,`SearchString`) VALUE (?,?);";
+
+        $sth = $dbh->prepare($sql);
+        $sth2 = $dbh->prepare($sql2);
+
+        for($i=0;$i<50;$i++)
+        {
+            echo $i;
+            sleep(1);
+            $guid = UUID::v4();
+
+            $sth->execute(array(
+                $guid,'0','0','0','0','0',$argv[1],'9','9'.$i
+            ));
+
+            $sth2->execute(array(
+                $guid,'0sdfgsdgdfgfdgdfgfdgdfgfdgfd'.$i
+            ));
+        }
+
+        $dbh->commit();
+
+    } catch (PDOException $e) {
+        $dbh->rollBack();
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+
+
+    print_r($argv);
 }
